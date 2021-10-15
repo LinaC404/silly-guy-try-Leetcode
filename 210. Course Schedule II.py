31a8095a1426df5253b0344f7d40324912c45a92
LinaChen404 <50405887+LinaC404@users.noreply.github.com>
2021-10-15 13:19:19 +0900

    Update 210. Course Schedule II.py

--- 210. Course Schedule II.py
+++ 210. Course Schedule II.py
@@ -10,6 +10,7 @@ class Solution(object):
         result = []
         CourseDict = defaultdict(list)
         indegress = defaultdict(int)
+        """Couse:[->next course]"""
         for Cour_idx,pre in prerequisites:
             CourseDict[pre].append(Cour_idx)
             indegress[Cour_idx] = indegress[Cour_idx]+1
@@ -33,9 +34,46 @@ class Solution(object):
         
         return result
 
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = defaultdict(list)
+        """Courcse: [prerequisite]"""
+        for u, v in prerequisites:
+            graph[u].append(v)
+        visited = [0] * numCourses
+        print(graph)
+        print(visited)
+        path = []
+        for i in range(numCourses):
+            if not self.dfs(graph, visited, i, path):
+                return []
+        return path
+    
+    def dfs(self, graph, visited, i, path):
+        # 0:not visitefd; 1: visiting; 2:safe
+        if visited[i] == 1: 
+            return False
+        if visited[i] == 2: 
+            return True
+        visited[i] = 1
+        for j in graph[i]:
+            #if the point is visiting, if yes, cycle is existed, return False
+            print(graph, visited, j, path)
+            if not self.dfs(graph, visited, j, path):
+                return False
+        visited[i] = 2
+        path.append(i)
+        return True
+
+
 if __name__=="__main__":
-    numCourses = 4
-    prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+    numCourses = 5
+    prerequisites = [[1,0],[2,0],[3,1],[3,2],[3,4],[4,3]]
     a = Solution()
     a.findOrder(numCourses, prerequisites)
 
