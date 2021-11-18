892b91f9209c08492d164e7a8092f57b1404d41f
LinaChen404 <50405887+LinaC404@users.noreply.github.com>
2021-11-18 22:26:29 +0900

    居然在八月份做过而且一点印象都没有 ，真尴尬11/18

--- Linked Lists/138. Copy List with Random Pointer.py
+++ Linked Lists/138. Copy List with Random Pointer.py
@@ -5,7 +5,6 @@ class Node:
         self.val = int(x)
         self.next = next
         self.random = random
-
 class Solution(object):
     def copyRandomList(self, head):
         """ HINT:
@@ -14,49 +13,29 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
-        curr = head
-        while curr:
-            curr_next = curr.next
-            copy_curr = Node(curr.val,curr_next,curr.random)
-            curr.next = copy_curr
-            copy_curr.next = curr_next
-            curr = curr_next
-            print(copy_curr)
+        if not head: return None
+        p1 = head
+        # copy the same next
+        while p1:
+            copy = Node(p1.val,p1.next,p1.random)
+            copy.next = p1.next
+            p1.next = copy
+            p1 = copy.next
 
-        # while p2:
-        #     print(p2.val)
-        #     print(p2.next)
-        #     p2 = p2.next
-        p2 = head.next
+        # copy the random
+        p2 = head
+        while p2:
+            p2.next.random = None if p2.random is None else p2.random.next
+            p2 = p2.next.next
+
         res = head.next
-        print(p2.val)
-        print("Next",p2.next)
-        print("Random",p2.random)
-        while p2.next:
-            the_next = p2.next.next
-            the_random = None if p2.random is None else p2.random.next
-            p2.next = the_next
-            p2.random = the_random
-            p2 = the_next
-            print(p2.val)
-            print("Next",p2.next)
-            print("Random",p2.random.val)
+        p3 = head
+        # remove the orininal
+        while p3:
+            node = p3.next
+            p3.next = node.next
+            node.next = None if node.next is None else node.next.next
+            p3 = p3.next
         return res
-            
 
-        
-if __name__=="__main__":
-    head = Node(7,Node(13,Node(11,Node(10,Node(1)))))
-    point_dict = defaultdict()
-    p = head
-    while p:
-        point_dict[p.val] = p
-        p = p.next
-    print(point_dict)
-    point_dict[7].random = None
-    point_dict[13].random = point_dict[7]
-    point_dict[11].random = point_dict[1]
-    point_dict[10].random = point_dict[11]
-    point_dict[1].random = point_dict[7]
-    a = Solution()
-    a.copyRandomList(head)
+            
