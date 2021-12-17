7bd2c0bc6122d706b5cf4bf4f912d4f1c882e2c3
LinaChen404 <50405887+LinaC404@users.noreply.github.com>
2021-12-17 10:35:10 +0900

    12/17 better version

--- Recursion/247. Strobogrammatic Number II.py
+++ Recursion/247. Strobogrammatic Number II.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def findStrobogrammatic(self, n):
+    def myfindStrobogrammatic(self, n):
         """
         :type n: int
         :rtype: List[str]
@@ -17,11 +17,7 @@ class Solution(object):
                 res.append(s)
                 return
             for char1,char2 in my_dict.items():
-                # s = s[:int(len(s)/2)]+char1+char2+s[int(len(s)/2):]
-                # print(s)
                 stro(_N+2,s[:int(len(s)/2)]+char1+char2+s[int(len(s)/2):])
-
-
             
         res = []
         ans = []
@@ -40,7 +36,34 @@ class Solution(object):
                     ans.append(res[j][:int(len(res[j])/2)]+m+res[j][int(len(res[j])/2):])
             return ans
 
+    def findStrobogrammatic(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        Runtime: 162 ms, faster than 72.73% of Python online submissions for Strobogrammatic Number II.
+        Memory Usage: 24 MB, less than 68.60% of Python online submissions for Strobogrammatic Number II.
+        The better code 
+        """
+        def inner(x,y):
+            #  return "" whenn is even/ return ["0","1","8"] when n is odd
+            if x == 0: return [""]
+            if x == 1: return ["0","1","8"]
+            
+            # I almost did not use return ans like it
+            partial = inner(x-2,y)
+            
+            ret = []
+            
+            for one_combo in partial:
+                # To avoid add 0 at the beginning of string
+                if x != y: ret.append("0{}0".format(one_combo))
+                ret.append("1{}1".format(one_combo))
+                ret.append("6{}9".format(one_combo))
+                ret.append("9{}6".format(one_combo))
+                ret.append("8{}8".format(one_combo))
+            return ret
 
+        return inner(n,n)
 
 
 if __name__=="__main__":
