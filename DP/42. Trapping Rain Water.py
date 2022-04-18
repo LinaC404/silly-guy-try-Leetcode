056c7cb3ac78df179c7f682c5a22ac21dc1c52aa
LinaChen404 <50405887+LinaC404@users.noreply.github.com>
2022-04-18 13:36:43 +0900

    2022/04/18 DAY9

--- DP/42. Trapping Rain Water.py
+++ DP/42. Trapping Rain Water.py
@@ -19,6 +19,28 @@ class Solution(object):
         return res
 # ---------------------------------------------------- Two pointer
 
+    def trap2(self, height: List[int]) -> int:
+        """
+        Runtime: 104 ms, faster than 88.22% of Python3 online submissions for Trapping Rain Water.
+        Memory Usage: 15.7 MB, less than 62.27% of Python3 online submissions for Trapping Rain Water.
+        """
+        ans = 0
+        l = 0
+        r = len(height)-1
+        l_bar = height[l]
+        r_bar = height[r]
+        #  短板效应
+        while l<r:
+            if l_bar<=r_bar:
+                ans += l_bar - height[l]
+                l += 1
+                l_bar = max(l_bar,height[l])
+            else:
+                ans += r_bar-height[r]
+                r -= 1
+                r_bar = max(r_bar,height[r])
+        return ans
+
                     
 if __name__=="__main__":
     a = Solution()
